fe9da5d6ef2cb91a5d179730e017831e2348ff02
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-04-10 12:22:48 +0200

    Update Game.py

--- game_basics/Game.py
+++ game_basics/Game.py
@@ -1,6 +1,10 @@
 import pygame as pg
+import os
 import sys
 from objects import Rocket
+from objects import Obst
+from collision import Col
+import time
 
 class Game(object):
     def __init__(self):
@@ -12,7 +16,20 @@ class Game(object):
         self.tps_clock=pg.time.Clock()
         self.tps_delta=0.0
         self.player=Rocket(self)
+        self.obstacles=Obst(self)
+        self.physics=Col(self)
+        pg.display.set_caption("Locket Rauncher")
+        self.bg = pg.image.load(os.path.join('images', 'mg2.jpg')).convert()
+        self.bgX=0
+        self.bgX2=self.bg.get_height()
         while True:
+            self.draw()
+            self.bgX += 1.1
+            self.bgX2 += 1.1
+            if self.bgX > self.bg.get_height() * 1:
+                self.bgX = -self.bg.get_height()
+            if self.bgX2 > self.bg.get_height() * 1:
+                self.bgX2 = -self.bg.get_height()
             for event in pg.event.get():
                 # Quitting
                 if event.type == pg.QUIT:
@@ -23,15 +40,20 @@ class Game(object):
                 self.tick()
                 self.tps_delta -= 1 / self.tps_max
             #Rendering
-            self.screen.fill((0, 0, 0))
-            self.draw()
-            pg.display.flip()
+            pg.display.update()
+            if (self.player.pos.y > 720 or self.player.pos.y < 0 or self.player.pos.x < 1280/12 or self.player.pos.x > 1180):
+                self.physics.messenger("Game Over",(255,0,0))
+                pg.display.update()
+                time.sleep(1)
+                exit(0)
     def tick(self):
         # Input
         self.player.tick()
-
     def draw(self):
         # Drawing
+        self.screen.blit(self.bg, (0,self.bgX))
+        self.screen.blit(self.bg, (0,self.bgX2))
         self.player.draw()
+        self.obstacles.draw()
 if __name__ == "__main__":
     Game()
