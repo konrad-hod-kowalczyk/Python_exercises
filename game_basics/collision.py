e4e3c9d2f7c78a54a418768d26586b33dca2e999
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-04-16 09:24:47 +0200

    Update collision.py

--- game_basics/collision.py
+++ game_basics/collision.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import time
 
 class Col(object):
     def __init__(self,game):
@@ -8,8 +9,21 @@ class Col(object):
         size = self.game.screen.get_size()
         text = font.render(msg, True, color)
         self.game.screen.blit(text, [size[0]/8,size[1]/3])
+        pg.display.update()
+        f = open("highscore.txt", "r+")
+        if(self.game.count>int(f.read())):
+            f.seek(0,0)
+            f.write(str(self.game.count))
+        f.close()
+        time.sleep(1)
+        exit(0)
     def score(self,count):
         font = pg.font.SysFont(None, 100)
         size = self.game.screen.get_size()
         text = font.render(str(count), True, (0,255,0))
         self.game.screen.blit(text, [0, size[1]/3])
+        f = open("highscore.txt", "r")
+        font = pg.font.SysFont(None, 50)
+        text = font.render("HS" + f.read(), True, (0, 255, 0))
+        f.close()
+        self.game.screen.blit(text, [0, size[1] / 2])
