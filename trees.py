2e92785eb5efcaf5a4ed0679999d5f9af3c0b9a3
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-06-13 22:37:52 +0200

    Update trees.py

--- trees.py
+++ trees.py
@@ -3,4 +3,8 @@ import random
 import math
 size=math.floor(random.random() * 50)
 class branch():
-  def __init__(self,number,table=[]):
+  def __init__(self,num,tab):
+    self.number=num
+    self.tuple=[]
+    for i in range(len(tab)):
+      self.tuple.append(tab[i])
