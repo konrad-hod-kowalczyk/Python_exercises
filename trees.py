2ce254f0cd15cc8f2421a89037252a704b3f281b
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-06-14 22:46:05 +0200

    Update trees.py

--- trees.py
+++ trees.py
@@ -1,10 +1,5 @@
 import pygame as pg
 import random
 import math
+from branches import branch
 size=math.floor(random.random() * 50)
-class branch():
-  def __init__(self,num,tab):
-    self.number=num
-    self.tuple=[]
-    for i in range(len(tab)):
-      self.tuple.append(tab[i])
