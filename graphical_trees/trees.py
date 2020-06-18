c97369b501b8483555ef16dee6ce7de3d2f4be32
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-06-18 19:07:03 +0200

    Update trees.py

--- graphical_trees/trees.py
+++ graphical_trees/trees.py
@@ -2,14 +2,18 @@ import pygame as pg
 import random
 import math
 from branches import branch
-size=math.floor(random.random() * 50)
-tab=[]
-for i in range(size):
-  tab.append(i+1)
-points=[];
 def initialisation():
+  global points #???
+  table.remove(n)
   help=[];
   h=size;
   rang=math.floor(random.random() * h)
   for i in range(rang):
     help.append(tab[math.floor(random.random() * h)])
+  for i in len(help):
+    initialisation(table,help[i])
+size=math.floor(random.random() * 50)
+tab=[]
+for i in range(size):
+  tab.append(i+1)
+points=[];
