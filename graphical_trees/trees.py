f6d4cd9227f80d4ab2d214f3d17e6e2bda2a8c62
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-06-24 09:26:09 +0200

    Update trees.py

--- graphical_trees/trees.py
+++ graphical_trees/trees.py
@@ -3,20 +3,22 @@ import random
 import math
 from branches import branch
 def initialisation(table,n,s):
-  global points #???
-  table.remove(n)
-  help=[]
-  h=s
-  rang=math.floor(random.random() * h)
-  for i in range(rang):
-      t=math.floor(random.random() * h)
-      help.append(table[t])
-  points.append(branch(n,help))
-  for i in range(len(help)):
-      initialisation(table,help[i],h-rang)
+    global points #???
+    table.remove(n)
+    help=[]
+    h=s-1
+    rang=math.floor(random.random() * h)
+    for i in range(rang):
+        t=math.floor(random.random() * h)
+        help.append(table[t])
+    points.append(branch(n,help))
+    for i in range(len(help)):
+        initialisation(table,help[i],h)
 size=math.floor(random.random() * 50)
 tab=[]
 for i in range(size):
     tab.append(i+1)
+    print(tab)
 points=[]
 initialisation(tab,tab[0],size)
+print(points.num)
