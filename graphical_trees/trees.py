f628d1990d010be16b26a53ff8bd348c491911dc
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-07-01 21:41:42 +0200

    Update trees.py

--- graphical_trees/trees.py
+++ graphical_trees/trees.py
@@ -1,21 +1,21 @@
-import pygame as pg
 import random
 import math
 from branches import branch
-def initialisation(n,s,rang):
+def initialisation(n,rang):
     global points
     global table
+    global banned
     table.remove(n)
     if rang<=0 or len(table)==0:
         return 0
     if rang>len(table):
         rang=len(table)
-    print("------------")
-    print("table=",table)
+    #print("------------")
+    #print("table=",table)
     help=[]
     h=len(table)
-    print("h=",h)
-    print("range=", rang)
+    #print("h=",h)
+    #print("range=", rang)
     #rang=math.floor(random.random() * (h-1))
     tabtab=[]
     for i in range(len(table)):
@@ -25,44 +25,46 @@ def initialisation(n,s,rang):
             rang=len(help)
             break
         h -= 1
-        print("****")
-        print("h=", h)
+        #print("****")
+        #print("h=", h)
         t=math.floor(random.random() * h)
-        print("t=",t)
-        print("table[t]=", tabtab[t])
+        if tabtab[t] in banned:
+            h+=1
+            continue
+        #print("t=",t)
+        #print("table[t]=", tabtab[t])
         help.append(tabtab[t])
-        print("help=",help)
+        #print("help=",help)
+        banned.append(tabtab[t])
         tabtab.remove(tabtab[t])
-        print("table=",tabtab)
+        #print("table=",tabtab)
     points.append(branch(n,help))
     for i in range(len(help)):
         if i>=len(help):
             break
-        r = math.floor(random.random() * (h - rang+2))
-        print("r=",r)
-        print("help=",help)
-        print("i=",i)
-        print("help[i]=", help[i])
-        print("table=",table)
+        r = math.floor(random.random() * (h - rang+2))+1
+        #print("r=",r)
+        #print("help=",help)
+        #print("i=",i)
+        #print("help[i]=", help[i])
+        #print("table=",table)
         rang+=r
-        if help[i] not in table:
-            help.remove(help[i])
-            continue
-        initialisation(help[i],h,r)
+        #if help[i] not in table:
+         #   help.remove(help[i])
+         #   continue
+        initialisation(help[i],r)
 size=math.floor(random.random() * 50)+1
 #size=5
 table=[]
 for i in range(size):
     table.append(i+1)
-    #print(tab)
-points=[]
-rr=math.floor(random.random()*(size-1))+1
-print("first range=",rr)
 print(table)
-initialisation(table[0],size,rr)
-print("########################################")
+points=[]
+banned=[]
+rr=math.floor(random.random()*(size/2))+1
+#print("first range=",rr)
+initialisation(table[0],rr)
+#print("########################################")
 for i in range(len(points)):
     print(points[i].number)
     print(points[i].tuple)
-#pg.init()
-#pg.quit()
