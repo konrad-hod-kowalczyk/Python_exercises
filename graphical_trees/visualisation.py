a1a6cfdc076088b4ac11d84e03e51890ceaf2141
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-07-08 20:36:50 +0200

    Update visualisation.py

--- graphical_trees/visualisation.py
+++ graphical_trees/visualisation.py
@@ -5,19 +5,21 @@ screen = pg.display.set_mode((1280,720))
 class visualisation():
     def __init__(self,pointstab,tab):
         self.lines=[]
-        self.lines.append(len(tab)+1)
+        tab.append(1)
+        self.lines.append(tab)
         for i in range(len(pointstab)):
-            if i==0:
+            if i == 0:
                 self.lines.append(pointstab[i].tuple)
             else:
-                help=[];
-                h=i;
+                help = []
+                counter=0
                 for j in range(len(pointstab)):
-                    if pointstab[h].number in pointstab[j].tuple and pointstab[j].number==1:
+                    if pointstab[i].number in pointstab[j].tuple and pointstab[j].number == 1:
                         help.append(pointstab[i].tuple)
-                    i=j
+                        i = j
+                        counter+=1
+
                 self.lines.append(help)
-                
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
