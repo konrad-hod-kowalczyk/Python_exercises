cec430862705dc69b009f974a5d3130b029429d8
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-07-14 21:46:59 +0200

    Update trees.py

--- graphical_trees/trees.py
+++ graphical_trees/trees.py
@@ -11,13 +11,8 @@ def initialisation(n,rang,max):
         return 0
     if rang>len(table):
         rang=len(table)
-    #print("------------")
-    #print("table=",table)
     help=[]
     h=len(table)
-    #print("h=",h)
-    #print("range=", rang)
-    #rang=math.floor(random.random() * (h-1))
     tabtab=[]
     for i in range(len(table)):
         tabtab.append(table[i])
@@ -26,8 +21,6 @@ def initialisation(n,rang,max):
             rang=len(help)
             break
         h -= 1
-        #print("****")
-        #print("h=", h)
         t=math.floor(random.random() * h)
         if tabtab[t] in banned:
             t=-1
@@ -38,29 +31,16 @@ def initialisation(n,rang,max):
         if tabtab[t] in banned:
             h-=1
             continue
-        #print("t=",t)
-        #print("table[t]=", tabtab[t])
         help.append(tabtab[t])
-        #print("help=",help)
         banned.append(tabtab[t])
         tabtab.remove(tabtab[t])
-        #print("table=",tabtab)
     points.append(branch(n,help))
     for i in range(len(help)):
         if i>=len(help) or len(table)==0:
             break
         h = len(table)
         r = math.floor(random.random() * (h-rang))
-        #print("r=",r)
-        #print("help=",help)
-        #print("i=",i)
-        #print("help[i]=", help[i])
-        #print("table=",table)
-        #print("banned=",banned)
         rang+=r
-        #if help[i] not in table:
-         #   help.remove(help[i])
-         #   continue
         initialisation(help[i],r,max)
 size=math.floor(random.random() * 50)+1
 size=10
@@ -71,9 +51,7 @@ print(table)
 points=[]
 banned=[]
 rr=math.floor(random.random()*(size/2))+1
-#print("first range=",rr)
 initialisation(table[0],rr,table[size-1])
-#print("########################################")
 for i in range(len(points)):
     print(points[i].number)
     print(points[i].tuple)
