0793e6496eaa388e67bce883d53261f3dddb41f6
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-07-17 20:35:09 +0200

    Update visualisation.py

--- graphical_trees/visualisation.py
+++ graphical_trees/visualisation.py
@@ -5,26 +5,26 @@ screen = pg.display.set_mode((1280,720))
 class visualisation():
     def __init__(self,pointstab,tab):
         self.verses=[]
-        tab.append(1)
-        self.verses.append(tab)
-        for i in range(len(pointstab)):
-            if i == 0:
+        tab.append(1)  #appending 1 to the top verse, as tree is always beginning with 1 in this program
+        self.verses.append(tab)  #appending to verse table
+        for i in range(len(pointstab)): #for elements in table do this
+            if i == 0: #if i==0 meaning it's the first element of tree, append all points connected with it
                 self.verses.append(pointstab[i].tuple)
             else:
                 help = []
-                for j in range(len(pointstab)):
-                    parent=-1
-                    for k in range(len(self.verses)):
-                        if pointstab[j].number in self.verses[k]:
-                            parent=k
-                    if pointstab[i].number in pointstab[j].tuple and parent!=-1:
-                        for k in range(len(pointstab[i].tuple)):
+                for j in range(len(pointstab)): #for every element in table with points in the tree and their connections
+                    parent=-1                    #set the parent to a non-existent one, number in table
+                    for k in range(len(self.verses)):  #in already existing verses search for the parent
+                        if pointstab[j].number in self.verses[k]:  #if the number is in existing verses then that's the parent
+                            parent=k 
+                    if pointstab[i].number in pointstab[j].tuple and parent!=-1: #if number of point in tree table is in tuple of another number and parent exists
+                        for k in range(len(pointstab[i].tuple)):  #append every point in this tuple to help table 
                             help.append(pointstab[i].tuple[k])
-                self.verses.append(help)
+                self.verses.append(help) #append help verses as new verse
         self.height=720/len(self.verses)
         print(self.verses)
-        points=[]
-        h=10
+        points=[] #table for coordinates on screen for every point
+        h=10 #height for first point on the screen
         for i in range(len(self.verses)):
             w = len(self.verses[i])
             if w * 50 > 640:
