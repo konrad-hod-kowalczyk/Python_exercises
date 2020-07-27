e3f62ab42fbd55f9abdf7974157dbd05fcc79eff
Illygynoth <62249323+Illygynoth@users.noreply.github.com>
2020-07-27 20:19:34 +0200

    Update branches.py

--- graphical_trees/branches.py
+++ graphical_trees/branches.py
@@ -1,6 +1,6 @@
 class branch():
-  def __init__(self,num,tab):
-    self.number=num
+  def __init__(self,num,tab):  #class for each branch in tree, number of point and tuple of points it's the parent of
+    self.number=num 
     self.tuple=[]
     for i in range(len(tab)):
       self.tuple.append(tab[i])
